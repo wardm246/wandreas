1bce2bff6429561fd895e90e071e47867073530f
AndreasStgm <andreassteegmans@hotmail.com>
2021-11-18 20:57:45 +0100

    Basis naar .csv schrijven werkend in nodejs

--- Andreas/TTN_Backend/TtnMQTTDownlink.js
+++ Andreas/TTN_Backend/TtnMQTTDownlink.js
@@ -1,26 +1,47 @@
-const mqtt = require('mqtt') //https://github.com/essadji/MQTT/blob/master/lora.js
+const mqtt = require('mqtt'); // https://www.npmjs.com/package/mqtt
+const fastcsv = require('fast-csv'); // https://www.npmjs.com/package/fast-csv
+const fs = require('fs'); // https://fedingo.com/how-to-export-to-csv-in-nodejs/
+//-------------------------
+const ws = fs.createWriteStream('data.csv');
 
-const host = 'eu1.cloud.thethings.network'
-const port = '1883'
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+//----------
+const host = 'eu1.cloud.thethings.network';
+const port = '1883';
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
-const connectUrl = `mqtt://${host}:${port}`
+const connectUrl = `mqtt://${host}:${port}`;
 const client = mqtt.connect(connectUrl, {
     clientId,
-    clean: true,
-    connectTimeout: 4000,
+    clean: true, //true, set to false to receive QoS 1 and 2 messages while offline
+    connectTimeout: 4000, // milliseconds, time to wait before a CONNACK is received
     username: 'webcontroller2021@ttn',
     password: 'NNSXS.FXH2NTABN3CTNTOXPBDFSTEYU3AGLGUUPXSXC5Q.4ZFHPRENLKLANGOPNBZADZITVJDXPKYUCIO5E4E4XNA4EEMK5ENA',
-    reconnectPeriod: 1000,
-})
+    reconnectPeriod: 1000, //  interval between two reconnections. Disable auto reconnect by setting to 0.
+});
+
+//-------------------------
 
 client.on('connect', () => {
-    console.log(`Client connected with client ID: ${clientId}`)
+    console.log(`Client connected with client ID: ${clientId}`);
     client.subscribe(['v3/+/devices/+/up'], () => {
         console.log(`Subscribed to topic: v3/+/devices/+/up`)
-    })
-})
+    });
+});
 client.on('message', (topic, payload) => {
-    const jsonPayload = JSON.parse(payload)
-    console.log('Message received by client on topic: ', topic, '\nPayload: ', jsonPayload.uplink_message.decoded_payload)
-})
+    const jsonPayload = JSON.parse(payload);
+    const jsonData = [
+        {
+            id: 1,
+            name: 'Temperature',
+            value: jsonPayload.uplink_message.decoded_payload.Temp
+        },
+        {
+            id: 2,
+            name: "Relative Humidity",
+            value: jsonPayload.uplink_message.decoded_payload.RelHumid
+        }];
+    console.log('Message received by client on topic: ', topic, '\nPayload: ', jsonPayload.uplink_message.decoded_payload);
+    fastcsv.write(jsonData, { headers: true }).on('finish', function () {
+        console.log('Write to CSV successful');
+    }).pipe(ws);
+});
