e5f0ec94481bf4db821837ea9efcc7525c061837
AndreasStgm <andreassteegmans@hotmail.com>
2021-12-09 20:42:17 +0100

    Plotly Test werkt

--- Andreas/TTN_Backend/DataVisualisation.js
+++ Andreas/TTN_Backend/DataVisualisation.js
@@ -23,6 +23,40 @@ const parse = fastcsv.parse(
     .on('data', readRow => console.log(readRow))
     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`))
 
+//----------Plotly test----------
+var traceTemp = {
+    x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
+    y: [40, 50, 60],
+    name: 'Temperature (°C)',
+    type: 'scatter'
+}
+var traceRelHumid = {
+    x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
+    y: [70, 80, 90],
+    name: 'Relative Humidity (%)',
+    yaxis: 'y2',
+    type: 'scatter'
+}
+var data = [traceTemp, traceRelHumid]
+
+var layout = {
+    title: 'Beehive Data',
+    yaxis: { title: 'yAxis Title' },
+    yaxis2: {
+        title: 'yAxis2 Title',
+        titlefont: { color: 'rgb(255, 25, 25)' },
+        tickfont: { color: 'rgb(255, 25, 25)' },
+        overlaying: 'y',
+        side: 'right'
+    }
+}
+
+var graphtOptions = { layout: layout, filename: 'visualisation_beehive', fileopt: 'overwrite' }
+plotly.plot(data, graphtOptions, function (err, msg) {
+    console.error(err)
+    console.log(msg)
+})
+
 //-------------------------
 const watch = fs.watchFile(dataFile, function () {
     console.log('data.csv was changed')
