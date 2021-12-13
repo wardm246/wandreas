fd739960ae627f146e6c4e7820f787921c9668e1
AndreasStgm <andreassteegmans@hotmail.com>
2021-12-13 10:46:27 +0100

    Parsing werkt nu WEL bij elke verandering

--- Andreas/TTN_Backend/DataVisualisation.js
+++ Andreas/TTN_Backend/DataVisualisation.js
@@ -1,5 +1,4 @@
 const express = require('express') //used for hosting the website
-
 const path = require('path') //used for sending the html
 
 const fastcsv = require('fast-csv') //used for parsing the csv data
@@ -7,32 +6,45 @@ const fs = require('fs')
 
 const plotly = require('plotly')('AndreasStgm', 'SvqNUM9cumNHtkrE2GOt') //used for plotting the data
 
-//-------------------------
+//----------Express Front-End----------
 
 const app = express()
 const port = 3000
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/visualisationPage.html'))
+})
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
+
+//----------Parsing CSV----------
+
 const dataFile = './data.csv'
-const parse = fastcsv.parse(
-    {
+
+const watch = fs.watchFile(dataFile, () => {
+    console.log('data.csv was changed')
+
+    const rs = fs.createReadStream(dataFile);
+    fastcsv.parseStream(rs, {
         ignoreEmpty: true,
         discardUnmappedColumns: true,
         headers: true
-    })
-    .on('error', error => console.error(error))
-    .on('data', readRow => console.log(readRow))
-    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`))
+    }).on('error', error => console.error(error))
+        .on('data', readRow => console.log(readRow))
+        .on('end', rowCount => console.log(`Parsed ${rowCount} rows`))
+})
 
-//----------Plotly test----------
+//----------Plotly----------
 var traceTemp = {
     x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
-    y: [40, 50, 60],
+    y: [40, 30, 50],
     name: 'Temperature (°C)',
     type: 'scatter'
 }
 var traceRelHumid = {
     x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
-    y: [70, 80, 90],
+    y: [50, 100, 150],
     name: 'Relative Humidity (%)',
     yaxis: 'y2',
     type: 'scatter'
@@ -41,11 +53,9 @@ var data = [traceTemp, traceRelHumid]
 
 var layout = {
     title: 'Beehive Data',
-    yaxis: { title: 'yAxis Title' },
+    yaxis: { title: 'Temperature (°C)' },
     yaxis2: {
-        title: 'yAxis2 Title',
-        titlefont: { color: 'rgb(255, 25, 25)' },
-        tickfont: { color: 'rgb(255, 25, 25)' },
+        title: 'Relative Humidity (%)',
         overlaying: 'y',
         side: 'right'
     }
@@ -53,20 +63,6 @@ var layout = {
 
 var graphtOptions = { layout: layout, filename: 'visualisation_beehive', fileopt: 'overwrite' }
 plotly.plot(data, graphtOptions, function (err, msg) {
-    console.error(err)
     console.log(msg)
 })
 
-//-------------------------
-const watch = fs.watchFile(dataFile, function () {
-    console.log('data.csv was changed')
-    const rs = fs.createReadStream(dataFile, { autoClose: true })
-        .pipe(parse)
-})
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/visualisationPage.html'))
-})
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
-})
